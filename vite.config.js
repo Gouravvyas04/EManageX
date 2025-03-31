fe8975cf052481cebc22ec78207abdd2359263e7
Gouravvyas14 <gouravvys510@gmail.com>
2025-03-31 12:58:01 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Correct base path for Render deployment
-  build: {
-    outDir: "dist", // Ensure build files go into the correct directory
-    assetsDir: "assets", // Keeps assets organized
-  },
-  server: {
-    port: 3000, // Optional: Set a local dev port
-  },
+  // build: {
+  //   outDir: "dist", // Ensure build files go into the correct directory
+  //   assetsDir: "assets", // Keeps assets organized
+  // },
+  // server: {
+  //   port: 3000, // Optional: Set a local dev port
+  // },
 });
